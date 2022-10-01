0b8480a41728746d25869066983514d4559bbde3
PHC <lucidsun92@gmail.com>
2022-10-01 14:33:25 +0900

    first commit

---
+++ TestProject2020/TestProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 대체로 무난하지만 벽붙기를 사용할 수 있음 (벽을 계속 밀면 아래로 안떨어지고 버틸 수 있음)
+
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpHeight;
+
+    public Rigidbody2D rb;
+    float inputDirection;
+
+    Vector2 velocity;
+
+    bool jump = false;
+    bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputDirection = Input.GetAxis("Horizontal");
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isGrounded)
+                jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = rb.velocity;
+        if (jump)
+        {
+            velocity.y += Mathf.Sqrt(2f * -Physics2D.gravity.y * jumpHeight);
+            jump = false;
+        }
+        velocity.x = inputDirection * speed;
+        
+        rb.velocity = velocity;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("TriggerEnter: " + collision.name);
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("TriggerExit: " + other.name);
+        isGrounded = false;
+    }
+}
---
+++ TestProject2020/TestProject/Assets/Scripts/PlayerControllerNonPhysics.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerNonPhysics : MonoBehaviour
+{
+    // 리지드 바디는 사용하되 움직임 적용은 리지드 바디로 하지 않고 transform으로 움직임
+    // 벽붙기는 방지할 수 있으나 쉬버링 현상 나타남 (Update와 물리처리간 시간차에 의한 것으로 보임)
+
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpHeight;
+
+    Rigidbody2D rigidbody;
+    float inputDirection;
+
+    bool jump = false;
+    bool isGrounded = true;
+
+    Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector2.zero;
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+
+        inputDirection = Input.GetAxis("Horizontal");
+        velocity.x = inputDirection * speed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                velocity.y += Mathf.Sqrt(2f * -Physics2D.gravity.y * jumpHeight);
+            }
+        }
+
+        Vector2 moved = velocity * Time.deltaTime;
+
+        // 물리 적용하지 않고 움직임
+        transform.position = (rigidbody.position + moved);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("TriggerStay: " + collision.name);
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("TriggerExit: " + other.name);
+        isGrounded = false;
+    }
+}
